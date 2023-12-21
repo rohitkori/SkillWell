d0e55b20c704a4819c5d881fcc8f2aa9058767f7
Rohit <kori.1@iitj.ac.in>
2023-12-21 19:29:33 +0530

    connecting mysql db using gorm

--- go-backend/models/index.go
+++ go-backend/models/index.go
@@ -2,7 +2,6 @@ package models
 
 import (
     "fmt"
-
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
 )
@@ -20,9 +19,19 @@ var DB *gorm.DB
 
 func InitDB(cfg Config) {
 
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode)
+    createDBDsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/", cfg.User, cfg.Password, cfg.Host, cfg.Port)
+    database, err := gorm.Open(mysql.Open(createDBDsn), &gorm.Config{})
 
+    // Create Database if it doesn't exists
+    _ = database.Exec("CREATE DATABASE IF NOT EXISTS " + cfg.DBName + ";")
+
+    //postgresql dsn
+    // dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode)
+  
+    //mysql dsn
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
     if err != nil {
         panic(err)
     }
